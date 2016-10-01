9ebfd5ff8037506be0286178531e2a1f446496f1
Mikey <mjosephmiller.uk@gmail.com>
2016-10-01 13:53:33 +0100

    set up search bar

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
-const API_KEY = 'AIzaSyDatznD96WTAxacQ-r-Db2Ip-guDN7QRKk';
 
+const API_KEY = YOUTUBE_API_KEY;
+
+YTSearch({key: API_KEY, term: 'surfboards'}),
 const App = () => {
   return (
     <div>
