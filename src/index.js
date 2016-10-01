e7b05551983d65a95a803d433fa39dc7d87350c5
Mikey <mjosephmiller.uk@gmail.com>
2016-10-01 16:39:50 +0100

    can select a video from the list

--- src/index.js
+++ src/index.js
@@ -12,11 +12,16 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'Mike Miller'}, (videos) => {
-      this.setState({ videos });
-      //when the key and the value have the same string it can be condensed. this.setState({ videos: videos })
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -24,8 +29,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
