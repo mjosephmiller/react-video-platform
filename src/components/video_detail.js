6c96f4a0d0f2d762fca826885d6be36b02dfd508
Mikey <mjosephmiller.uk@gmail.com>
2016-10-01 17:13:24 +0100

    fix typo affecting css

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -9,7 +9,7 @@ const VideoDetail = ({video}) => {
 
   return (
     <div className="video-detail col-md-8">
-    <div clssName="embed-responsive embed-responsive-16by9">
+    <div className="embed-responsive embed-responsive-16by9">
         <iframe className="embed-responsive-item" src={url}></iframe>
     </div>
     <div className="details">
